21707b81cc855a26415aa75ffaa7d0e5181c23f6
Marco Cerilli <cerillimarco15@gmail.com>
2025-09-30 20:42:22 +0200

    Forza ambiente di produzione in index.php

--- public/index.php
+++ public/index.php
@@ -2,11 +2,11 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php'; // QUESTA RIGA DEVE ESSERE QUI
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php'; // QUESTA RIGA DEVE ESSERE QUI
 
 // La riga per i trusted proxies va dopo, se presente
 // Esempio: Symfony\Component\HttpFoundation\Request::setTrustedProxies(...)
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel('prod', false);
 };
