def59f8b8353c1dea2acba9fd40664f089f3bc21
Marco Cerilli <cerillimarco15@gmail.com>
2025-09-30 19:27:15 +0200

    Fix: Corretto ordine di boot in index.php

--- public/index.php
+++ public/index.php
@@ -2,10 +2,8 @@
 
 use App\Kernel;
 
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 Symfony\Component\HttpFoundation\Request::setTrustedProxies(['127.0.0.1', 'REMOTE_ADDR'], Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_FOR | Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PORT | Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_PROTO);
-
-
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
